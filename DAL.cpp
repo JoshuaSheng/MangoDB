e68269c0f0bbaa729a4824150ac65793d28d9830
Joshua Sheng <jsdragonok2@gmail.com>
2023-08-19 03:40:17 -0400

    created data access layer

---
+++ DAL.cpp
@@ -0,0 +1,103 @@
+//
+// Created by Josh on 2023-08-19.
+//
+
+#include "DAL.h"
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <cstdint>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+typedef uint64_t pgnum;
+typedef unsigned char BYTE;
+
+struct freelist {
+    pgnum maxPage;
+    vector<pgnum> releasedPages;
+
+    freelist(pgnum initialPage) {
+        maxPage = initialPage;
+    }
+
+    pgnum getNextPage() {
+        if (releasedPages.size() != 0) {
+            pgnum pageID = releasedPages.back();
+            releasedPages.pop_back();
+            return pageID;
+        }
+        maxPage += 1;
+        return maxPage;
+    }
+};
+
+struct page {
+    pgnum num;
+    int size;
+    vector<BYTE> *data;
+};
+
+struct dal {
+    string path;
+    int pagesize;
+    fstream *file;
+    freelist *freeList;
+
+    page *allocateEmptyPage() {
+       return new page {0, this->pagesize, new vector<BYTE>{}};
+    };
+
+    page *readPage(pgnum pageNum) {
+        page *p = allocateEmptyPage();
+        auto offset = int(pageNum) * pagesize;
+        file->seekg(int64_t(offset));
+        //don't eat new lines in binary mode
+        file->unsetf(std::ios::skipws);
+
+        p->data->reserve(p->size);
+        p->data->insert(p->data->begin(),
+                        istreambuf_iterator<char>(*file),
+                        istreambuf_iterator<char>());
+        return p;
+    }
+
+    void writePage(page *p) {
+        auto offset = int(p->num) * pagesize;
+        file->seekg(offset);
+        file->write((char *)p->data->data(), p->size);
+        if (file -> fail()) {
+            cerr << "Error: path " << path << " failed to write";
+        }
+    }
+
+    void close() {
+        if (file != nullptr) {
+            file->close();
+            if (file->fail()) {
+                cerr << "Error: path " << path << " failed to close";
+            }
+            delete file;
+            file = nullptr;
+        }
+        else {
+            cerr << "Error: path " << path << " failed to close";
+        }
+    }
+};
+
+dal *open(string path, int pageSize) {
+    dal *new_dal = new dal{path, pageSize, nullptr, new freelist{0}};
+    fstream *new_file = new fstream(path, ios::binary);
+
+    new_dal->file = new_file;
+    new_dal->path = path;
+
+    new_dal->file->open(path);
+    if (new_dal->file->fail()) {
+        cerr << "Error: path " << path << " failed to open";
+    }
+    return new_dal;
+}
---
+++ DAL.h
@@ -0,0 +1,14 @@
+//
+// Created by Josh on 2023-08-19.
+//
+
+#ifndef NOTMYSQL_DAL_H
+#define NOTMYSQL_DAL_H
+
+
+class DAL {
+
+};
+
+
+#endif //NOTMYSQL_DAL_H
